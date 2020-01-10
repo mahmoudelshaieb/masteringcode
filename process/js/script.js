dee756cdefe43c7ec7093de04316218feaf679f2
elshaieb <elshaieb_career@outlook.com>
2020-01-10 21:35:46 +0200

    okay done

--- process/js/script.js
+++ process/js/script.js
@@ -1,32 +1,10 @@
-var artists, calendar;
 
-function loadData(url) {
-  return new Promise(function (resolve, reject) {
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function () {
-      if (req.status == 200) {
-        console.log('getting ' + url);
-        resolve(JSON.parse(req.response))
-      } else {
-        reject(Error('Problem loading data'))
-      }
-    } // onload callback
-    req.send();
-  });
-}
+var action = document.querySelectorAll('[data-action]');
 
-calendar = loadData('js/data_schedule.json');
-artists = loadData('js/data_artists.json');
+action.forEach(element => {
+  element.addEventListener('click', function() {
+    console.log(element);
+  })
+});
 
-document.querySelector('.load-info')
-  .addEventListener('click', function () {
-    calendar.then(function (data) {
-      console.log(data);
-      return artists;
-    }, function (error) {
-      console.log('Problem', error);
-    }).then(function (otherData) {
-      console.log(otherData);
-    });
-  });
+alert('aaa')
